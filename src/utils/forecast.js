55915e425138a39aabe708dd40d744a0cc533cbe
Sayantan Karmakar <sayantank22@gmail.com>
2019-11-12 13:17:36 +0530

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -25,7 +25,7 @@ const forecast = (lat, long, callback) => {
         }else if(body.error){
             callback('Unable to find loaction, please check the given url', undefined)
         }else{
-            callback(undefined, body.daily.data[0].summary + " It is currently " + body.currently.temperature+" degrees here. There is "+body.currently.precipProbability + "% chance of rain")
+            callback(undefined, body.daily.data[0].summary +" It is currently " + body.currently.temperature+" degrees here. The high today is " + body.daily.data[0].temperatureHigh + " with a low of " + body.daily.data[0].temperatureLow +". There is "+body.currently.precipProbability + "% chance of rain")
         }
     })
 }
